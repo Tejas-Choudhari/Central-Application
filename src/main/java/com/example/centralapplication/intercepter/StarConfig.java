a34c5041bf210c8defa1e2cad798000cab8b7ace
installuser <Tejas-hub-ttc>
2023-11-22 12:51:56 +0530

    fixed some bug

--- src/main/java/com/example/centralapplication/intercepter/StarConfig.java
+++ src/main/java/com/example/centralapplication/intercepter/StarConfig.java
@@ -1,27 +1,27 @@
-package com.example.centralapplication.intercepter;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-//class to configuration of the intercepter that what controller should implement the intercepter
-@Configuration
-public class StarConfig implements WebMvcConfigurer {
-    @Bean
-    public Intercepter intercepter() {
-        return new Intercepter();
-    }
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-
-        registry.addInterceptor(intercepter());
-
-    }
-    //    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(Intercepter());
+//package com.example.centralapplication.intercepter;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+////class to configuration of the intercepter that what controller should implement the intercepter
+//@Configuration
+//public class StarConfig implements WebMvcConfigurer {
+//    @Bean
+//    public Intercepter intercepter() {
+//        return new Intercepter();
 //    }
-
-}
-
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//
+//        registry.addInterceptor(intercepter());
+//
+//    }
+//    //    public void addInterceptors(InterceptorRegistry registry) {
+////        registry.addInterceptor(Intercepter());
+////    }
+//
+//}
+//
